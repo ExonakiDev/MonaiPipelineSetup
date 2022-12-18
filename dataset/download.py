27716c23ca0550f53b6db436b5a30b8f56a23efd
Rohit Sanyal <rohit.sanyal@monachilpartners.com>
2022-12-18 06:34:10 -0500

    changed dataset.json

--- dataset/download.py
+++ dataset/download.py
@@ -4,18 +4,16 @@ import requests
 
 from dataset.download_gcp import authenticate_implicit_with_adc, download_blob
 
-# from download_gcp import download_blob, authenticate_implicit_with_adc
-# from utils.parse_json import parse_dataset_json_file
-
 def download_dataset_from_json ():
     """Parses datasets.json and downloads all the datasets listed in the file to their respective filepaths."""
-
-    # google authentication step needed if we are downloading data from google storage bucket.
-    authenticate_implicit_with_adc("fluid-tangent-363120")
 
     # read in json file
     f = open('datasets.json')
     data = json.load(f)
+
+    # google authentication step needed if we are downloading data from google storage bucket.
+    # print(data['datasets'][0])
+    authenticate_implicit_with_adc(data['datasets'][0]['project_name'])
 
     # iterate over datasets in .json file
     for i, dataset in enumerate(data['datasets']):
@@ -25,15 +23,19 @@ def download_dataset_from_json ():
 
         # create dir for dataset to be stored in
         destination_file_path = '/'.join(destination_file_name.split('/')[:-1])
-        os.makedirs(destination_file_path)
+        if not os.path.exists(destination_file_path):
+            os.makedirs(destination_file_path)
 
         # make google storage api call to download the blob to our dir
-        download_blob(bucket_name, blob_name, destination_file_name, "fluid-tangent-363120")
+        download_blob(bucket_name, blob_name, destination_file_name, data['datasets'][0]['project_name'])
 
 def download_public_data_from_url ():
     # read in json file
     f = open('datasets.json')
     data = json.load(f)
+
+    if not os.path.exists('datasets'):
+        os.makedirs('datasets')
 
     # iterate over datasets in .json file
     for i, dataset in enumerate(data['public_datasets']):
@@ -42,6 +44,6 @@ def download_public_data_from_url ():
         response = requests.get(url)
         open(name, "wb").write(response.content)
 
-        os.replace(name, "../custom_data/file.foo")
+        os.replace(name, "datasets/{}".format(name))
 
 download_dataset_from_json ()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataset.download import download_public_data_from_url
+from dataset.download import download_dataset_from_json
 
 # download_dataset_from_json()
 download_public_data_from_url()
