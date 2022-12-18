51260fb25e79774ed0b4b8951821dde123686383
Rohit Sanyal <rohit.sanyal@monachilpartners.com>
2022-12-18 06:43:24 -0500

    Update startmonaicustom.sh

--- startmonaicustom.sh
+++ startmonaicustom.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ..
 docker run -p 8080:8000 --gpus all --rm -ti --ipc=host --net=host projectmonai/monailabel:latest bash -c '
-mkdir custom_data
-cd custom_data
-curl https://storage.googleapis.com/heart_data/Task03_Liver.tar -o custom_data/liver.tar
+mkdir datasets
+cd datasets
+curl https://storage.googleapis.com/heart_data/Task03_Liver.tar -o datasets/liver.tar
 tar -xvf liver.tar
 cd ..
 monailabel apps --download --name radiology --output apps
-monailabel start_server --app apps/radiology --studies custom_data/Task03_Liver/imagesTr --conf models deepedit'
+monailabel start_server --app apps/radiology --studies datasets/Task03_Liver/imagesTr --conf models deepedit'
